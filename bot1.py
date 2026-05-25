604881a8ab0f84fe74c55050886291db430f6cbe
anoanov <anoanov919@gmail.com>
2026-05-25 06:07:57 +0500

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,41 +1,41 @@
+
 import os
-import telebot
-from yt_dlp import YoutubeDL
+import yt_dlp
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils.executor import start_polling
 
-TOKEN = "7633395197:AAHeSWP4wCkJHlVfvr5_Z3ec_2zV0aIOcr8"
-bot = telebot.TeleBot(TOKEN)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Salom! Linki YouTube firist")
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
 
-@bot.message_handler(func=lambda m: True)
-def download(message):
-    url = message.text.strip()
-    
-    if "youtube.com" not in url and "youtu.be" not in url:
-        bot.send_message(message.chat.id, "Faqat linki YouTube firist")
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.reply("Салом! Линки YouTube, Instagram ё TikTok-ро фирист, видеояшро мегирам.")
+
+@dp.message_handler()
+async def download(message: types.Message):
+    url = message.text
+    if "youtube.com" not in url and "youtu.be" not in url and "instagram.com" not in url and "tiktok.com" not in url:
+        await message.reply("Лутфан танҳо линки YouTube, Instagram ё TikTok фирист.")
         return
     
-    bot.send_message(message.chat.id, "Video bor meshavad...")
+    await message.reply("Видеоро мегирам, 1 дақиқа интизор шав...")
     
     try:
         ydl_opts = {
+            'format': 'mp4',
             'outtmpl': 'video.mp4',
-            'format': 'best[height<=720]',
-            'quiet': True
+            'quiet': True,
         }
-        
-        with YoutubeDL(ydl_opts) as ydl:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
         
-        with open('video.mp4', 'rb') as video:
-            bot.send_video(message.chat.id, video)
-        
+        await bot.send_video(message.chat.id, video=open('video.mp4', 'rb'))
         os.remove('video.mp4')
         
-    except:
-        bot.send_message(message.chat.id, "Khata: Video girifta nashud")
+    except Exception as e:
+        await message.reply(f"Хато шуд: {e}")
 
-if __name__ == "__main_":
-    bot.polling(none_stop=True)
+if name == 'main':
+    start_polling(dp, skip_updates=True)
